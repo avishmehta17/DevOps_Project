45361a00ba3be8a592e4854bcbefd23fcc5ed98f
Avish Jain <114907547+avishmehta17@users.noreply.github.com>
2025-09-30 22:40:10 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,26 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]  # Canonical's AWS account
+}
+
+resource "aws_instance" "dev_machine" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = "project-key"
+
+  tags = {
+    Environment = "dev"
+    Name        = "${var.name}-server"
+  }
+}
